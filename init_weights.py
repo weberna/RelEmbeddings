1a63c8e6f9020935054fdb5a09a934fb758d2ede
nwweber <nweb2000@gmail.com>
2016-12-16 21:04:37 -0500

    fixed init_weights for relations

--- init_weights.py
+++ init_weights.py
@@ -6,8 +6,13 @@ def init_weights(word_dict, other_dict, orig_embed, embed_size, using_rels=False
     weights = np.random.uniform(-1, 1, (len(word_dict.keys()), embed_size))
     for word in word_dict.keys():  #set the weights for all words in dictionary
         if using_rels: #if we are using relations, must put into corret format
-            word = word.replace(' ', '_')
-        if word in other_dict.keys(): #if this word has an embedding in orig_embed
+            my_rel = word
+            rel = word.replace(' ', '_')
+            if rel in other_dict.keys(): #if this word has an embedding in orig_embed
+                print("YEAH")
+                weights[word_dict[my_rel]] = orig_embed[other_dict[rel]] #init this embedding 
+
+        elif word in other_dict.keys(): #if this word has an embedding in orig_embed
             print("YEAH")
             weights[word_dict[word]] = orig_embed[other_dict[word]] #init this embedding 
     return weights
