9187482a0f9a05763126fbd328a5d755c1e14ecf
nwweber <nweb2000@gmail.com>
2016-12-16 20:36:05 -0500

    Added ability to use relations in init_weights

--- init_weights.py
+++ init_weights.py
@@ -2,9 +2,11 @@ import pickle as pkl
 import numpy as np
 import sys
 
-def init_weights(word_dict, other_dict, orig_embed, embed_size):
+def init_weights(word_dict, other_dict, orig_embed, embed_size, using_rels=False):
     weights = np.random.uniform(-1, 1, (len(word_dict.keys()), embed_size))
     for word in word_dict.keys():  #set the weights for all words in dictionary
+        if using_rels: #if we are using relations, must put into corret format
+            word = word.replace(' ', '_')
         if word in other_dict.keys(): #if this word has an embedding in orig_embed
             print("YEAH")
             weights[word_dict[word]] = orig_embed[other_dict[word]] #init this embedding 
@@ -16,9 +18,13 @@ if __name__ == "__main__":
     other_dict_file = sys.argv[2]
     embed_file = sys.argv[3]
     out_file = sys.argv[4]
+    use_rels = sys.argv[5]
     word_dict = pkl.load(open(word_file, "rb"))
     embed_dict = pkl.load(open(other_dict_file, "rb"))
     embeddings = pkl.load(open(embed_file, "rb"))
     words_size, embed_size = embeddings.shape
-    weights = init_weights(word_dict, embed_dict, embeddings, embed_size)
+    if use_rels == "yes":
+        weights = init_weights(word_dict, embed_dict, embeddings, embed_size, True)
+    else:
+        weights = init_weights(word_dict, embed_dict, embeddings, embed_size)
     pkl.dump(weights, open(out_file,"wb"))
